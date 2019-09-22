c795ba2c2ca566a2ae5b391b242d131d52e65fb1
shr1kee <i.nalimov@ya.ru>
2019-09-22 20:42:01 +0300

    change

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var path = require('path');
-var request = require('request')
+var request = require('request-promise-native')
 var functions = require('firebase-functions')
 var {WebhookClient} = require('dialogflow-fulfillment')
 var {card, Suggestion } = require('dialogflow-fulfillment')
@@ -29,12 +29,15 @@ app.post('/dialogflow', express.json(), (req, res)=> {
 
     function test(agent) {
        // agent.add('welcome to my server!!!! ^_^');
-        request('https://api.themoviedb.org/3/trending/all/week?api_key=0f0f4e4397fdd57b3f38401d074f4dff', {json: true}, function(error, response, body) {
-            var result = body['results'][0].title
-            console.log('123')
-            agent.add(JSON.stringify(result));
+        return request.get('https://api.themoviedb.org/3/trending/all/week?api_key=0f0f4e4397fdd57b3f38401d074f4dff')
+            .then(body => {
+                return agent.add(body['results'][0].title);
         });
-
+        // request(, {json: true}, function(error, response, body) {
+        //     var result =
+        //     console.log('123')
+        //     agent.add(JSON.stringify(result));
+        // });
     }
 
     let intentMap = new Map();
